b3a9a80455dc7fc449a448e9bc113bbbf93f84b9
kafka404 <kafkadani1237@gmail.com>
2026-05-28 22:35:36 +0700

    Update InterfaceDAO.java

--- Medify/src/Model/Medify/InterfaceDAO.java
+++ Medify/src/Model/Medify/InterfaceDAO.java
@@ -3,11 +3,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Model.Medify;
-
+import java.util.List;
 /**
  *
  * @author USER
  */
-public class InterfaceDAO {
-    
+public interface InterfaceDAO {
+    public void insert(Object obj);
+    public void update(Object obj);
+    public void delete(int id);
+    public Object getById(int id);
+    public List<Object> getAll();
 }
