21fc62000218a73caf80bff90c7138da057d1027
Vinz_ <putraalvinandhika@gmail.com>
2026-06-01 20:59:48 +0700

    Update ObatBiasa.java

--- Medify/src/Model/Medify/ObatBiasa.java
+++ Medify/src/Model/Medify/ObatBiasa.java
@@ -8,6 +8,32 @@ package Model.Medify;
  *
  * @author USER
  */
-public class ObatBiasa {
+public class ObatBiasa extends ModelObat {
+    // Kelas Anak 
+    private int harga;
     
+    // Constructor 
+    public ObatBiasa(int idObat , String namaObat , int stok , int harga){
+        super(idObat , namaObat , stok); // Panggil Constructor Di Kelas Induk
+        this.harga = harga;
+    }
+    
+    @Override // Mewarisi Sifat Dan Perilaku Kelas Induk
+    public int getHarga(){
+        return harga;
+    }
+    
+    @Override
+    public double getDiskon(){
+        return 0 ; // Obat Biasa Ga Dapet Diskon
+    }
+    
+    @Override
+    public String getJenis(){
+        return "Biasa";
+    }
+    
+    public void setHarga(int harga){
+        this.harga = harga;
+    }
 }
