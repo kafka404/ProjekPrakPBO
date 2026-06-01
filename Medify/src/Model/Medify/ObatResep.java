b05f0306f7c487e27ac9b58c3822f9ce805cf1fe
Vinz_ <putraalvinandhika@gmail.com>
2026-06-01 21:00:43 +0700

    Implement ObatResep class with pricing and discount

--- Medify/src/Model/Medify/ObatResep.java
+++ Medify/src/Model/Medify/ObatResep.java
@@ -8,6 +8,30 @@ package Model.Medify;
  *
  * @author USER
  */
-public class ObatResep {
+public class ObatResep extends ModelObat {
+    private int harga;
     
+    public ObatResep(int idObat , String namaObat , int stok , int harga){
+        super(idObat , namaObat , stok);
+        this.harga = harga;
+    }
+    
+    @Override 
+    public int getHarga(){
+        return harga;
+    }
+    
+    @Override
+    public double getDiskon(){
+        return 10; // Obat Resep Dapet Diskon 10%
+    }
+    
+    @Override 
+    public String getJenis(){
+       return "Resep";
+    }
+    
+    public void setHarga(int harga){
+        this.harga = harga;
+    }
 }
