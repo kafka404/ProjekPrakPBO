94e736a1a65e1596cb760fcc0207ef4b726c7323
Vinz_ <putraalvinandhika@gmail.com>
2026-06-01 20:59:31 +0700

    Refactor ModelObat to be an abstract class with properties
    Added fields and methods for managing medication details.

--- Medify/src/Model/Medify/ModelObat.java
+++ Medify/src/Model/Medify/ModelObat.java
@@ -8,6 +8,52 @@ package Model.Medify;
  *
  * @author USER
  */
-public class ModelObat {
+public abstract class ModelObat {
+    // Kelas Abstract
     
+    private int idObat;
+    private String namaObat;
+    private int stok;
+    
+    // Constructor
+    public ModelObat(int idObat , String namaObat , int stok){
+        this.idObat = idObat;
+        this.namaObat = namaObat;
+        this.stok = stok;
+    }
+    
+    // Abstract Method Untuk Kelas Anak
+    public abstract int getHarga();
+    public abstract double   getDiskon();
+    public abstract String getJenis();
+    
+    // Method Untuk Harga Obat Setelah Diskon
+    public int getHargaDiskon(){
+        return (int)(getHarga() * (1 - getDiskon()/100));
+    }
+    
+    // Getter dan Setter
+    public int getIdObat(){
+        return idObat;
+    }
+    
+    public void setIdObat(int idObat){
+        this.idObat = idObat;
+    }
+    
+    public String getNamaObat(){
+        return namaObat;
+    }
+    
+    public void setNamaObat(String namaObat){
+        this.namaObat = namaObat;
+    } 
+    
+    public int getStok(){
+        return stok;
+    }
+    
+    public void setStok(int stok){
+        this.stok = stok;
+    }
 }
