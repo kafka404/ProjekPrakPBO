9e35f9ce24044842900df8b975163209c2edf495
Vinz_ <putraalvinandhika@gmail.com>
2026-06-02 21:17:13 +0700

    Update medify.sql
    Added new entries to the 'obat' table and created the 'transaksi' table with its structure and primary key.

--- medify.sql
+++ medify.sql
@@ -17,16 +17,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `medify`
---
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `obat`
---
-
 CREATE TABLE `obat` (
   `id_obat` int(11) NOT NULL,
   `nama_obat` varchar(100) NOT NULL,
@@ -35,9 +25,7 @@ CREATE TABLE `obat` (
   `jenis` enum('Biasa','Resep') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data untuk tabel `obat`
---
+
 
 INSERT INTO `obat` (`id_obat`, `nama_obat`, `harga`, `stok`, `jenis`) VALUES
 (1, 'Paracetamol', 10000, 50, 'Biasa'),
@@ -48,13 +36,19 @@ INSERT INTO `obat` (`id_obat`, `nama_obat`, `harga`, `stok`, `jenis`) VALUES
 (6, 'Tramadol', 50000, 20, 'Resep'),
 (7, 'Metformin', 35000, 25, 'Resep'),
 (8, 'Captopril', 20000, 15, 'Resep'),
-(9, 'Cefixime', 45000, 10, 'Resep');
+(9, 'Cefixime', 45000, 10, 'Resep'),
+(10, 'Asam Mefenamat 500 mg', 5000, 35, 'Resep'),
+(11, 'Omeprazole 20 mg', 15000, 10, 'Resep'),
+(12, 'Lansoprazole 30 mg', 20000, 15, 'Resep'),
+(13, 'Simvastatin 20 mg', 10000, 10, 'Resep'),
+(14, 'Cardio Aspirin', 38000, 20, 'Resep'),
+(15, 'Siladex Cough n Cold', 24000, 18, 'Biasa'),
+(16, 'Tolak Angin(Box)', 42000, 5, 'Biasa'),
+(17, 'Tolak Angin (Bungkus)', 4500, 30, 'Biasa'),
+(18, 'Lespain Cream 15g', 12700, 7, 'Biasa'),
+(19, 'FreshCare Roll On', 15500, 20, 'Biasa'),
+(20, 'Combantrin 250mg', 22000, 14, 'Biasa');
 
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `transaksi`
---
 
 CREATE TABLE `transaksi` (
   `id_transaksi` int(11) NOT NULL,
@@ -67,35 +61,18 @@ CREATE TABLE `transaksi` (
   `tanggal` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Indexes for dumped tables
---
-
---
--- Indeks untuk tabel `obat`
---
 ALTER TABLE `obat`
   ADD PRIMARY KEY (`id_obat`);
 
---
--- Indeks untuk tabel `transaksi`
---
+
 ALTER TABLE `transaksi`
   ADD PRIMARY KEY (`id_transaksi`);
 
---
--- AUTO_INCREMENT untuk tabel yang dibuang
---
 
---
--- AUTO_INCREMENT untuk tabel `obat`
---
 ALTER TABLE `obat`
   MODIFY `id_obat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
---
--- AUTO_INCREMENT untuk tabel `transaksi`
---
+
 ALTER TABLE `transaksi`
   MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
